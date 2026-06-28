b54d004b8c7de4160ecb80ddca0df844ad34f394
maximeliseyev <maxim7eli@gmail.com>
2026-06-28 10:43:35 +0300

    merge mls and channel services in one group service

--- channel-service/src/service.rs
+++
@@ -1,9 +0,0 @@
-use std::sync::Arc;
-
-use construct_server_shared::clients::mls::MlsClient;
-
-#[derive(Clone)]
-pub(crate) struct ChannelServiceImpl {
-    pub(crate) db: Arc<sqlx::PgPool>,
-    pub(crate) mls_client: Option<MlsClient>,
-}
